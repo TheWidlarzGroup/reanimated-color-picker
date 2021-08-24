c6e8510628151521a4e7509bb229e9bd3f87fdef
Magda <jaskowska.magda@gmail.com>
2021-08-24 15:05:28 +0100

    init commit

--- src/components/BubbleContainer.tsx
+++ src/components/BubbleContainer.tsx
@@ -1,13 +1,7 @@
 import React, {useState} from 'react';
 import {TouchableOpacity, useWindowDimensions, View} from 'react-native';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
 import {useNavigation} from '@react-navigation/native';
 import {Box, Text, mkUseStyles, theme} from '../utils/theme';
-import {randomFromRange} from '../utils/helpers';
 import IconBack from '../assets/icon-back-white.svg';
 import {Bubble} from './Bubble';
 import {COLORS} from '../utils/mockedColors';
@@ -28,27 +22,15 @@ export const BubbleContainer = () => {
   const {goBack} = useNavigation();
   const [dropColor, setDropColor] = useState(theme.colors.disabledText);
   const {width, height} = useWindowDimensions();
-  const dropTop = useSharedValue(height - 175);
-  const dropHeight = useSharedValue(1000);
 
   const initBubbles = COLORS.map(color => ({
     ...color,
     position: {
-      x: randomFromRange(56, width - 56),
-      y: randomFromRange(130, height - 250),
+      x: 0,
+      y: 0,
     },
   }));
   const [bubbles] = useState<BubbleProps[]>(initBubbles);
-
-  const animateDropArea = () => {
-    dropTop.value = withTiming(-50, {duration: 600});
-    dropHeight.value = withTiming(1.5 * height, {duration: 600});
-  };
-
-  const animatedDrop = useAnimatedStyle(() => ({
-    top: dropTop.value,
-    height: dropHeight.value,
-  }));
 
   return (
     <View style={styles.mainContainer}>
@@ -59,30 +41,27 @@ export const BubbleContainer = () => {
         <IconBack />
       </TouchableOpacity>
       <Box marginTop="xxxl" alignItems="center">
-        <Text marginHorizontal="xxl">
+        <Text marginHorizontal="xxl" color="white" textAlign="center">
           Pick your favourite color and drop it in the grey area below
         </Text>
       </Box>
-      <Animated.View
+      <View
         style={[
           styles.dropArea,
-          animatedDrop,
           {
             backgroundColor: dropColor,
+            top: height - 175,
             left: width / 2 - 500,
             zIndex: dropColor === theme.colors.disabledText ? 0 : 3,
+            height: 1000,
+            width: 1000,
+            borderRadius: 1000 / 2,
           },
         ]}
       />
       {bubbles.map(bubble => (
         <Box position="absolute" key={bubble.id}>
-          <Bubble
-            {...bubble}
-            diameter={56}
-            dropArea={dropTop.value}
-            setDropColor={setDropColor}
-            animateDropArea={animateDropArea}
-          />
+          <Bubble {...bubble} diameter={56} />
         </Box>
       ))}
     </View>
